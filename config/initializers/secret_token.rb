57e70ddac23444c53c298158f656d169ff3887d9
miio <info@miio.info>
2012-10-13 15:01:57 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartitionTest::Application.config.secret_token = 'c76cf3bd9177d9c89438cfe925bef619346d161362efdae8481fcb8bbb60b2ef842ff01bf441f01142d858903c82479e9d0bf2d17743f0be46ce43147bec323b'
